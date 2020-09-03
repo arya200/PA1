35c2372354bc7d12a1ba7afd0aa320ee73938ad2
arya200 <arya2000@tamu.edu>
2020-09-03 19:19:54 -0400

    added gettimeofday for 1000 data points. Added binary file reading and saving functionality does give difference for large fiel though

--- client.cpp
+++ client.cpp
@@ -13,11 +13,14 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
     FIFORequestChannel chan ("control", FIFORequestChannel::CLIENT_SIDE);
-    datamsg d = datamsg(0, 0, 0);
+    datamsg d = datamsg(15, 0.008, 2);
     int option;
     string filename = "";
-    bool single = false;
+    bool singlepoint = false;
+    bool multiple_points = false;
     //d.person = 15, d.ecgno=1, d.seconds=0.008;
     //cout << argc << endl;
 
@@ -27,10 +30,11 @@ int main(int argc, char *argv[])
         {
             case 'p':
                     d.person = stoi(optarg);
+                    multiple_points = true;
                     break;
             case 't':
                     d.seconds = stod(optarg);
-                    single = true;
+                    singlepoint = true;
                     break;
 
             case 'e':
@@ -42,7 +46,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    if(single)
+    if(singlepoint && multiple_points)
     {
         chan.cwrite(&d, sizeof(datamsg));
         double result;
@@ -50,7 +54,7 @@ int main(int argc, char *argv[])
         cout << "The result is:" << result << endl;
     
     }
-    else
+    if(multiple_points)
     {
         ofstream file("received/x1.csv");
         double seconds = 0;
@@ -67,43 +71,65 @@ int main(int argc, char *argv[])
             
         }
         file.close();
-        
+        gettimeofday(&end, NULL);
+        double time_taken; 
+  
+    time_taken = (end.tv_sec - start.tv_sec) * 1e6; 
+    time_taken = (time_taken + (end.tv_usec - start.tv_usec)) * 1e-6; 
+  
+    cout << "Time taken by program is : " << fixed << time_taken << setprecision(6); 
+    cout << " sec" << endl; 
     }
     
-
-    if(true)
+    //filename = "100.dat";
+    if(filename.size()>1)
     {
-        cout << "I make it here"
-         = "15.csv";
-        //int windowsize=;
+        ofstream file("received/" + filename);
+        //cout << "I enter here" << endl;
+        //int windowsize=30;
         filemsg f(0,0);
         char buf[sizeof(filemsg) + filename.size()+1];
         memcpy(buf, &f, sizeof(filemsg));
         //memcpy (buf + sizeof(filemsg), filename.c_str(), filename.size() + 1);
-        strcpy(buf + sizeof(buf), filename.c_str());
+        strcpy(buf + sizeof(filemsg), filename.c_str());
         chan.cwrite(buf, sizeof(buf));
 
         __int64_t filelen;
         chan.cread (&filelen, sizeof(__int64_t));
-        cout << filelen << endl;
-        // char recvbuf[windowsize];
-        // chan.cread(recvbuf, sizeof(recvbuf));
-        // cout << recvbuf << endl;
+        //cout << filelen << endl;
 
+        int count = ceil((double)filelen/256);
+        cout << "The ceil value is: " << count  << endl;
+        int offset_val = 0;
+        int length = 256;
+        char recvbuf[filelen];
+        while(count>0)
+        {
+            
+            if(offset_val + length > filelen)
+            {
+                length = filelen - offset_val;
+               
+            }
+             cout  << "The length is:"  << length << endl;
+             cout << "the offset val is:"  << offset_val << endl;
+             cout << "the count is:" << count  << endl;
+            filemsg f1(offset_val, length);
+            char buf1[sizeof(filemsg) + filename.size()+1];
+            memcpy(buf1, &f1, sizeof(filemsg));
+            //memcpy (buf + sizeof(filemsg), filename.c_str(), filename.size() + 1);
+            strcpy(buf1 + sizeof(filemsg), filename.c_str());
+            chan.cwrite(buf1, sizeof(buf1));
+            chan.cread(recvbuf, sizeof(recvbuf));
+            offset_val = offset_val + 256;
+            count--;
 
-       
+        }
+        file.write(recvbuf, filelen);
+        file.close(); 
+
     }
 
-
-
-
-
-
-    // sending a non-   sense message, you need to change this
-    // char buf [MAX_MESSAGE];
-    // char x = 55;
-    // chan.cwrite (&x, sizeof (x));
-    // int nbytes = chan.cread (buf, MAX_MESSAGE);
     // closing the channel    
     MESSAGE_TYPE m = QUIT_MSG;
     chan.cwrite (&m, sizeof (MESSAGE_TYPE));
